0a3ba9b2a6db5a4633a8c4d2458fc5b90e2ded6a
SambandhaRai <ginny_30@icloud.com>
2025-12-04 15:41:10 +0545

    splash screen done

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:blood_link/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'package:blood_link/screens/on_boarding_screen_1.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => OnBoardingScreen1()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Image.asset('assets/images/blood_link_logo_red.png')],
+        ),
+      ),
+    );
+  }
+}
