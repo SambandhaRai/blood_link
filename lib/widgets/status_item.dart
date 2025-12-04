48aa5707ba1e9fbcc2358d07b499160b3567f478
SambandhaRai <ginny_30@icloud.com>
2025-12-05 02:23:11 +0545

    home screen half done

---
+++ lib/widgets/status_item.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class StatusItem extends StatelessWidget {
+  final Widget icon;
+  final String status;
+  final String label;
+
+  const StatusItem({
+    super.key,
+    required this.icon,
+    required this.status,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        icon,
+        const SizedBox(width: 10),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              status,
+              style: const TextStyle(
+                fontFamily: 'Bricolage Grotesque',
+                fontSize: 18,
+                fontWeight: FontWeight.w700,
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              label,
+              style: const TextStyle(
+                fontFamily: 'Bricolage Grotesque',
+                fontSize: 18,
+                fontWeight: FontWeight.w300,
+                color: Colors.grey,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
