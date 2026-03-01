5664d1186fec3d7519247fe341343de4052c232a
SambandhaRai <ginny_30@icloud.com>
2026-03-01 20:19:15 +0545

    Updated user information fetch

--- lib/features/dashboard/presentation/widgets/status_card.dart
+++ lib/features/dashboard/presentation/widgets/status_card.dart
@@ -1,6 +1,4 @@
 import 'package:blood_link/core/services/storage/user_session_service.dart';
-import 'package:blood_link/features/bloodGroup/domain/entities/blood_entity.dart';
-import 'package:blood_link/features/bloodGroup/presentation/view_model/blood_group_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -17,29 +15,13 @@ class _StatusCardState extends ConsumerState<StatusCard> {
   @override
   void initState() {
     super.initState();
-    Future.microtask(() {
-      ref.read(bloodGroupViewModelProvider.notifier).getAllBloodGroups();
-    });
-  }
-
-  String _getBloodGroupNameById(
-    String? bloodId,
-    List<BloodEntity> bloodGroups,
-  ) {
-    if (bloodId == null) return 'Unknown';
-    try {
-      return bloodGroups.firstWhere((c) => c.bloodId == bloodId).bloodGroup;
-    } catch (e) {
-      return 'Unknown';
-    }
   }
 
   @override
   Widget build(BuildContext context) {
     final userSessionService = ref.read(userSessionServiceProvider);
-    final userBloodId = userSessionService.getCurrentUserBloodId();
-
-    final bloodState = ref.watch(bloodGroupViewModelProvider);
+    final bloodGroupName =
+        userSessionService.getCurrentUserBloodGroupName() ?? "Unknown";
 
     return Card(
       color: Colors.white,
@@ -60,10 +42,7 @@ class _StatusCardState extends ConsumerState<StatusCard> {
                     width: 50,
                     height: 50,
                   ),
-                  status: _getBloodGroupNameById(
-                    userBloodId,
-                    bloodState.bloodGroups,
-                  ),
+                  status: bloodGroupName,
                   label: 'Blood Group',
                 ),
               ),
